1aeaf3e2fc9ee597de0a855f760011e97dd0bd51
MocanuAlexandra <mocanu.alexandra1504@gmail.com>
2022-03-21 13:36:39 +0200

    Added icons for all the windows

--- Notepad/Notepad/obj/Debug/Notepad_Content.g.i.cs
+++ Notepad/Notepad/obj/Debug/Notepad_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icons/readonly.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icons/window_icon.png")]
 
 
